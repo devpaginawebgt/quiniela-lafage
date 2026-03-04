aaa8e78a6c4a8cbb2d5e94db91a059de1e44877c
Dennis Herrera <dennishumher25@hotmail.com>
2026-03-04 14:35:36 -0600

    fix: changed seeder regex

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -19,7 +19,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'GT',
             'area_code'              => '502',
             'document_name'          => 'DPI',
-            'document_regex'         => '/^[0-9]{13}$/',
+            'document_regex'         => '^[0-9]{13}$',
             'document_regex_message' => 'El número de DPI debe contener 13 dígitos.',
             'timezone'               => 'GMT-6',
             'is_active'              => true
@@ -31,7 +31,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'HN',
             'area_code'              => '504',
             'document_name'          => 'Cédula',
-            'document_regex'         => '/^[0-9]{13}$/',
+            'document_regex'         => '^[0-9]{13}$',
             'document_regex_message' => 'El número de cédula debe contener 13 dígitos.',
             'timezone'               => 'GMT-6',
             'is_active'              => true
@@ -43,7 +43,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'PA',
             'area_code'              => '507',
             'document_name'          => 'CIP',
-            'document_regex'         => '/^[A-Z0-9]{6,11}$/',
+            'document_regex'         => '^[A-Z0-9]{6,11}$',
             'document_regex_message' => 'El CIP debe contener entre 6 y 11 caracteres alfanuméricos.',
             'timezone'               => 'GMT-5',
             'is_active'              => true
@@ -55,7 +55,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'NI',
             'area_code'              => '505',
             'document_name'          => 'Cédula',
-            'document_regex'         => '/^[0-9]{13}[A-Z]$/',
+            'document_regex'         => '^[0-9]{13}[A-Z]$',
             'document_regex_message' => 'La cédula debe contener 13 dígitos y una letra mayúscula al final.',
             'timezone'               => 'GMT-6',
             'is_active'              => true
@@ -67,7 +67,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'CR',
             'area_code'              => '506',
             'document_name'          => 'Cédula',
-            'document_regex'         => '/^[0-9]{9}$/',
+            'document_regex'         => '^[0-9]{9}$',
             'document_regex_message' => 'El número de cédula debe contener 9 dígitos.',
             'timezone'               => 'GMT-6',
             'is_active'              => true
@@ -79,7 +79,7 @@ class CountrySeeder extends Seeder
             'country_code'           => 'DO',
             'area_code'              => '1',
             'document_name'          => 'Cédula',
-            'document_regex'         => '/^[0-9]{11}$/',
+            'document_regex'         => '^[0-9]{11}$',
             'document_regex_message' => 'El número de cédula debe contener 11 dígitos.',
             'timezone'               => 'GMT-4',
             'is_active'              => false
